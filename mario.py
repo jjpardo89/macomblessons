e557e35b7f1c671b021657c9ebf791289e92bdc6
jjpardo89 <jjpardo89@gmail.com>
2021-03-15 19:40:43 -0400

    Initial commit

---
+++ mario.py
@@ -0,0 +1,38 @@
+one_up_shroom = str()
+power_up_shroom = str()
+magic_shroom = str()
+feather = str()
+fire_flower = str()
+question = str()
+amount = int()
+
+#Hit the question box, what do you want?
+question = str(input("What do you hope will be in the QUESTION BOX?? Answer here: "))
+
+if question == "one up shrooms":
+    one_up_shroom = "You got duped, Goomba bitch"
+    print("Wrong, here is a Goomba, bitch!")
+elif question == "power up shrooms":
+    power_up_shrooms = "I'm BIG"
+    print("You big donger you")
+elif question == "magic shroom":
+    feather = "I can fly now, fruitcake"
+    print("You feathery flying bitch")
+elif question == "fire flower":
+    fire_flower = "Flamer coming through. Bitch."
+    print("Ha - you flaming faggot")
+else:
+    print("You aint no Mario bro!")
+
+second_question = str()
+
+second_question == str(input("Which Mario Bro do you love more? : "))
+
+if second_question == "mario":
+    one_up_shroom = "Spagetti!"
+    print("I like spaghetti")
+elif second_question == "luigi":
+    power_up_shrooms = "mustaschio"
+    print("I love Daisy")
+
+
